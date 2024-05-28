e6d0579592fdaf68517ac526952ca950714de129
Colin Bassett <colinbassett28@icloud.com>
2024-05-27 23:20:52 -0400

    version 0.0.2
    Added save and add buttons, added modals, changed JS a lot. Also changed some CSS

---
+++ assets/js/components/modal.js
@@ -0,0 +1,25 @@
+// Get the modal element
+const editModal = document.getElementById("editModal");
+const addModal = document.getElementById("addModal");
+
+// Get the <span> element that closes the modal
+const span = document.querySelectorAll(".close");
+
+// When the user clicks on <span> (x), close the modal
+span.forEach((closeBtn) => {
+  closeBtn.addEventListener('click', (e) => {
+    if (editModal.style.display == 'block') {
+      editModal.style.display = 'none';
+    } else {
+      addModal.style.display = 'none';
+    }
+  })
+})
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == editModal || event.target == addModal) {
+        editModal.style.display = "none";
+        addModal.style.display = "none";
+    }
+};
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,21 @@
 const edit = () => {
-  const todoContent = document.querySelector('.todo-content');
+  const todoList = document.querySelector("#todo-list");
+  let todoListItem = document.querySelectorAll('#todo-item');
   const todoActionsDiv = document.querySelector('.todo-actions');
   const todoActions = document.querySelectorAll('#todo-action');
-  const todoListItem = document.querySelectorAll('#todo-item');
+
+  const editModal = document.getElementById("editModal");
+  const addModal = document.getElementById("addModal");
+
+  const changeName = document.querySelector("#changeName");
+  const addTodo = document.querySelector("#addTodo");
   
   // loop through all the items in the todoListItem and add a checkbox to each list item
   for (let i = 0; i < todoListItem.length; i++) {
     const checkbox = document.createElement('input');
     checkbox.type = "checkbox";
-    checkbox.className = 'todo-checkbox';
+    checkbox.className = `todo-checkbox`;
+    checkbox.id = i;
     todoListItem[i].appendChild(checkbox);
   }
 
@@ -18,6 +25,23 @@ const edit = () => {
     const todoAction = todoActions[i];
     todoAction.style.display = 'none';
   }
+
+  // create the add button
+  let addBtn = document.createElement('button');
+  addBtn.className = 'todo-action add';
+  addBtn.id = 'todo-action';
+  addBtn.innerHTML = 'Add';
+
+  // add the add button to the todoActionsDiv
+  todoActionsDiv.appendChild(addBtn);
+
+  // get the new add button
+  addBtn = document.querySelector('.add');
+
+  // if the add button was clicked, show the addModal to create a new list item
+  addBtn.addEventListener('click', () => {
+    addModal.style.display = 'block';
+  })
 
   // create the save button
   let saveBtn = document.createElement('button');
@@ -34,6 +58,7 @@ const edit = () => {
   // if the save button was clicked, show the old buttons and remove the checkboxes
   saveBtn.addEventListener('click', () => {
     saveBtn.remove();
+    addBtn.remove();
 
     const todoCheckboxes = document.querySelectorAll('.todo-checkbox');
     for (let i = 0; i < todoCheckboxes.length; i++) {
@@ -48,4 +73,126 @@ const edit = () => {
       todoAction.style.display = 'inline-block';
     }
   })
+
+  // when the checkbox is clicked, remove the item and the checkbox
+  const todoCheckboxes = document.querySelectorAll('.todo-checkbox');
+  todoCheckboxes.forEach((checkbox) => {
+    checkbox.addEventListener('click', () => {
+      const checkboxNum = Number(checkbox.id);
+      if (isNaN(checkboxNum)) {
+        // do absolutely nothing
+        console.log('Checkbox ID is not a valid number');
+        return;
+      }
+      checkbox.remove();
+      todoListItem[checkboxNum].remove();
+    })
+  })
+
+  // if someone clicks on the todoItem allow them to edit the name
+  todoListItem.forEach((item) => {
+    item.addEventListener('click', (e) => {
+      // check if the Edit button is hidden if not do nothing
+      if (todoActions[0].style.display == 'none') {
+        /* 
+          We want to check to see if the Edit button is visible
+          because if it is visible that means the user is not
+          editing the list and the editModal should not open
+        */
+
+        // check if the input was clicked, if so do nothing. if not, show the editModal
+        if (e.target.nodeName === "INPUT") {
+          return;
+        } else {
+          editModal.style.display = "block";
+          editModal.className = `${item.classList} modal`;
+        }
+        
+        // edit the placeholder to show what list item was clicked.
+        const nameChangeField = document.getElementById('nameChange');
+        nameChangeField.placeholder = item.innerText;
+      } else {
+        return;
+      }
+    })
+  })
+
+  // if the changeName button was clicked, change the name
+  changeName.addEventListener('click', () => {
+    // the editModal.classList returns an array so we have to loop through it
+    for (let i = 0; i < editModal.classList.length; i++) {
+      // if the editModal.classList[i] isn't a number then do nothing
+      if (isNaN(editModal.classList[i])) {
+        return;
+      } else {
+        // if it isn't then we have the number of the todoListItem
+        const todoListNum = editModal.classList[i];
+        let nameChangeInput = document.querySelector("#nameChange").value;
+
+        // create another checkbox for that item
+        const checkbox = document.createElement('input');
+
+        checkbox.type = "checkbox";
+        checkbox.className = `todo-checkbox`;
+        checkbox.id = i;
+
+        // change the name and add the checkbox
+        todoListItem[todoListNum].innerHTML = nameChangeInput;
+        todoListItem[todoListNum].appendChild(checkbox);
+
+        // close the modal
+        editModal.style.display = 'none';
+
+        // if the checkbox is clicked, remove the todoListItem and the checkbox
+        checkbox.addEventListener('click', () => {
+          todoListItem[todoListNum].remove();
+          checkbox.remove();
+        })
+      }
+    }
+  })
+
+  // if the addTodo button was clicked, add a new todoItem to todoList
+  addTodo.addEventListener('click', () => {
+    let newNameInput = document.querySelector("#newName").value;
+    const lastTodoItem = todoListItem[todoListItem.length - 1];
+
+    // lastTodoItem returns an array so we have to loop through it
+    for (let i = 0; i < lastTodoItem.classList.length; i++) {
+      // if lastTodoItem.classList[i] returns a string, return
+      if (isNaN(lastTodoItem.classList[i])) {
+        return;
+      } else {
+        // if it gives us a number, create the new todoItem
+        console.log(i);
+        const todoNum = lastTodoItem.classList[i];
+        const newTodoItem = document.createElement('li');
+        newTodoItem.className = Number(todoNum) + 1;
+        newTodoItem.id = 'todo-item';
+        newTodoItem.innerHTML = newNameInput;
+
+        // create the new checkbox
+        const checkbox = document.createElement('input');
+
+        checkbox.type = "checkbox";
+        checkbox.className = `todo-checkbox`;
+        checkbox.id = i;
+
+        // append the checkbox and the newTodoItem
+        newTodoItem.appendChild(checkbox);
+        console.log(newTodoItem);
+        todoList.appendChild(newTodoItem);
+
+        // close the modal
+        addModal.style.display = 'none';
+
+        // if the checkbox is clicked remove the new item and the checkbox
+        checkbox.addEventListener('click', () => {
+          newTodoItem.remove();
+          checkbox.remove();
+        })
+      }
+    }
+  })
+  todoListItem = document.querySelectorAll('#todo-item');
 }
