61dc2ca2607a69a0bebe2a68b1427759328fee01
Colin Bassett <colinbassett28@icloud.com>
2024-06-04 09:27:59 -0400

    version 0.2.5
    Updated the firebase config

--- assets/js/components/firebase.js
+++ assets/js/components/firebase.js
@@ -1,8 +1,10 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 import {
   apiKey,
   authDomain,
+  databaseURL,
   projectId,
   storageBucket,
   messagingSenderId,
@@ -12,6 +14,7 @@ import {
 const firebaseConfig = {
   apiKey: apiKey,
   authDomain: authDomain,
+  databaseURL: databaseURL,
   projectId: projectId,
   messagingSenderId: messagingSenderId,
   appId: appId
@@ -19,3 +22,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app, storageBucket);
+export const database = getDatabase(app);
