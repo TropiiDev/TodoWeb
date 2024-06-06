067a5726463695bc3dadde34b6ded72b0694c4e0
Colin Bassett <colinbassett28@icloud.com>
2024-06-06 12:45:59 -0400

    version 0.3.1
    Implementing a fix that can allow users to add spaces in their todo name and it wont break code

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,9 +1,13 @@
-import { app, database } from './components/firebase.js';
-import { 
+import {
   getAuth,
-  onAuthStateChanged
+  onAuthStateChanged,
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { ref, set, onValue, push } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import {
+  onValue,
+  ref,
+  set,
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import { app, database } from "./components/firebase.js";
 
 const auth = getAuth();
 
@@ -21,195 +25,222 @@ onAuthStateChanged(auth, (user) => {
         let todoName = [];
         // get all the names of the objects
         for (let key in data) {
-          todoName.push(key);
+          if (key.indexOf(' ') > 0) {
+            const newTodoName = key.replace(/ /g, '+');
+            todoName.push(newTodoName);
+          } else {
+            todoName.push(key);
+          }
         }
 
         // loop through all the items in todoName
         for (let i = 0; i < todoName.length; i++) {
           // get the newTodosRef
           const newTodosRef = ref(database, `users/${user.uid}/${todoName[i]}`);
-        onValue(newTodosRef, (newSnapshot) => {
-          // get the newData
-          const newData = newSnapshot.val();
-          // get the description & todos of each object
-          const todoDescription = Object.values(newData)[0];
-          const todos = Object.values(newData)[1];
+          onValue(newTodosRef, (newSnapshot) => {
+            // get the newData
+            const newData = newSnapshot.val();
+            // get the description & todos of each object
+            const todoDescription = Object.values(newData)[0];
+            const todos = Object.values(newData)[1];
 
-          // get the sectionHolder to append each section to
-          const sectionHolder = document.querySelector('.section-holder');
+            // get the sectionHolder to append each section to
+            const sectionHolder = document.querySelector(".section-holder");
 
-          // create the new todo list
-          const todoSection = document.createElement('section');
-          todoSection.className = 'todo';
-          sectionHolder.appendChild(todoSection);
+            // create the new todo list
+            const todoSection = document.createElement("section");
+            todoSection.className = "todo";
+            sectionHolder.appendChild(todoSection);
 
-          // create the div of the todo
-          const todoDiv = document.createElement('div');
-          todoDiv.className = `todo-div ${todoName[i]}`;
+            // create the div of the todo
+            const todoDiv = document.createElement("div");
+            todoDiv.className = `todo-div ${todoName[i]}`;
 
-          // set the name of the todos
-          const todoH2 = document.createElement('h2');
-          todoH2.className = `title ${todoName[i]}`;
-          todoH2.innerHTML = todoName[i];
-          todoDiv.appendChild(todoH2);
+            // set the name of the todos
+            const todoH2 = document.createElement("h2");
+            todoH2.className = `title ${todoName[i]}`;
+            todoH2.innerHTML = todoName[i];
+            todoDiv.appendChild(todoH2);
 
-          // set the description
-          const todoP = document.createElement('p');
-          todoP.className = `description ${todoName[i]}`;
-          todoP.innerHTML = todoDescription
-          todoDiv.appendChild(todoP);
+            // set the description
+            const todoP = document.createElement("p");
+            todoP.className = `description ${todoName[i]}`;
+            todoP.innerHTML = todoDescription;
+            todoDiv.appendChild(todoP);
 
-          // create the div that holds the ul
-          const todoContent = document.createElement('div');
-          todoContent.className = 'todo-content';
-          todoDiv.appendChild(todoContent);
+            // create the div that holds the ul
+            const todoContent = document.createElement("div");
+            todoContent.className = "todo-content";
+            todoDiv.appendChild(todoContent);
 
-          // create the ul that holds the items
-          const todoUl = document.createElement('ul');
-          todoUl.className = todoName[i];
-          todoUl.id = 'todo-list';
-          todoContent.appendChild(todoUl);
+            // create the ul that holds the items
+            const todoUl = document.createElement("ul");
+            todoUl.className = todoName[i];
+            todoUl.id = "todo-list";
+            todoContent.appendChild(todoUl);
 
-          // loop through all the todos
-          for (let todo in todos) {
-            const todoName = Object.values(todos)[todo].name;
-  
-            const taskItem = document.createElement('li');
-            taskItem.className = i;
-            taskItem.innerHTML = todoName;
-            taskItem.id = 'todo-item';
-            todoUl.appendChild(taskItem);
-          }
+            // loop through all the todos
+            for (let todo in todos) {
+              const todoName = Object.values(todos)[todo].name;
 
-          // create the todoActionsDiv
-          const todoActionsDiv = document.createElement('div');
-          todoActionsDiv.className = `todo-actions-${todoName[i]}`;
-          todoActionsDiv.style.margin = '0';
-          todoActionsDiv.style.padding = '0';
-          todoDiv.appendChild(todoActionsDiv);
+              const taskItem = document.createElement("li");
+              taskItem.className = i;
+              taskItem.innerHTML = todoName;
+              taskItem.id = "todo-item";
+              todoUl.appendChild(taskItem);
+            }
 
-          // create the Edit button
-          const todoEditBtn = document.createElement('button');
-          todoEditBtn.className = 'todo-action';
-          todoEditBtn.id = todoName[i];
-          todoEditBtn.innerHTML = 'Edit';
-          todoActionsDiv.appendChild(todoEditBtn);
+            // create the todoActionsDiv
+            
+            const todoActionsDiv = document.createElement("div");
+            todoActionsDiv.className = 'todo-actions';
+            todoActionsDiv.id = `${todoName[i]}-actions`;
+            todoActionsDiv.style.margin = "0";
+            todoActionsDiv.style.padding = "0";
+            todoDiv.appendChild(todoActionsDiv);
+            
+            // create the Edit button
+            const todoEditBtn = document.createElement("button");
+            todoEditBtn.className = "todo-action";
+            todoEditBtn.id = todoName[i];
+            todoEditBtn.innerHTML = "Edit";
+            todoActionsDiv.appendChild(todoEditBtn);
 
-          // create the Delete button
-          const todoDeleteBtn = document.createElement('button');
-          todoDeleteBtn.className = 'todo-action';
-          todoDeleteBtn.id = todoName[i]
-          todoDeleteBtn.innerHTML = "Delete";
-          todoDeleteBtn.style.marginLeft = '5px';
-          todoActionsDiv.appendChild(todoDeleteBtn);
+            // create the Delete button
+            const todoDeleteBtn = document.createElement("button");
+            todoDeleteBtn.className = "todo-action";
+            todoDeleteBtn.id = todoName[i];
+            todoDeleteBtn.innerHTML = "Delete";
+            todoDeleteBtn.style.marginLeft = "5px";
+            todoActionsDiv.appendChild(todoDeleteBtn);
 
-          // append all that to the todoSection
-          todoSection.appendChild(todoDiv);
-
-        })
+            // append all that to the todoSection
+            todoSection.appendChild(todoDiv);
+          });
         }
       } else {
         // the user has no lists
-        console.info('User has no todo lists');
+        console.info("User has no todo lists");
       }
 
       // when the user clicks a todoActionBtn, do that action
-      const actionBtns = document.querySelectorAll('.todo-action');
+      const actionBtns = document.querySelectorAll(".todo-action");
       actionBtns.forEach((actionBtn) => {
-        actionBtn.addEventListener('click', () => {
+        actionBtn.addEventListener("click", () => {
           const btnName = actionBtn.innerHTML;
           const btnId = actionBtn.id;
           if (btnName === "Edit") {
             editTodo(user.uid, btnId);
           }
-        })
-      })
-    })
+        });
+      });
+    });
 
     // when the user clicks the createTodoBtn, show the modal.
-    const createTodoBtn = document.querySelector('#createTodoButton');
+    const createTodoBtn = document.querySelector("#createTodoButton");
 
-    createTodoBtn.addEventListener('click', () => {
-      const createNewTodoModal = document.querySelector('#createNewTodoModal');
-      const createNewTodoBtn = document.querySelector('#createTodo');
+    createTodoBtn.addEventListener("click", () => {
+      const createNewTodoModal = document.querySelector("#createNewTodoModal");
+      const createNewTodoBtn = document.querySelector("#createTodo");
       // show the modal
-      createNewTodoModal.style.display = 'flex';
+      createNewTodoModal.style.display = "flex";
 
       // when the user clicks the createNewTodoBtn, create the new todo
-      createNewTodoBtn.addEventListener('click', () => {
-        const newTodoName = document.querySelector('#newTodoName');
-        const newTodoDescription = document.querySelector('#newTodoDescription');
-        const firstTask = document.querySelector('#firstTask');
+      createNewTodoBtn.addEventListener("click", () => {
+        const newTodoName = document.querySelector("#newTodoName");
+        const newTodoDescription = document.querySelector(
+          "#newTodoDescription",
+        );
+        const firstTask = document.querySelector("#firstTask");
 
+        if (newTodoName.value.indexOf(' ') > 0) {
+          const newName = newTodoName.value.replace(/ /g, '+');
+          console.log(newName)
+
+          set(ref(database, `users/${user.uid}/${newName}`), {
+            description: newTodoDescription.value,
+            todos: {
+              0: {
+                name: firstTask.value,
+              },
+            },
+          });
+
+          window.location.reload();
+
+          return;
+        }
+        
         set(ref(database, `users/${user.uid}/${newTodoName.value}`), {
           description: newTodoDescription.value,
           todos: {
             0: {
-              "name": firstTask.value
-            }
-          }
-        })
+              name: firstTask.value,
+            },
+          },
+        });
 
         window.location.reload();
-      })
-    })
+      });
+    });
   } else {
     // not signed in
-    window.location.href = './signin.html';
+    window.location.href = "./signin.html";
     return;
   }
-})
+});
 
 const editTodo = (uid, name) => {
   // hide the old buttons
-  console.log(name)
+  console.log(name);
   const todoActions = document.querySelectorAll(`#${name}`);
-  const todoActionsDiv = document.querySelectorAll(`.todo-actions-${name}`);
+  const todoActionsDiv = document.querySelectorAll(`#${name}`);
 
+  console.log(todoActionsDiv);
+
+  /*
   for (let i = 0; i < todoActions.length; i++) {
     const todoAction = todoActions[i];
     if (todoAction.id === name) {
-      todoAction.style.display = 'none';
+      todoAction.style.display = "none";
     } else {
       return;
     }
   }
 
-  console.log(todoActionsDiv)
-
   // create the add and save buttons
-  const addBtn = document.createElement('button');
-  addBtn.className = 'todo-action';
-  addBtn.id = 'add';
-  addBtn.innerHTML = 'Add';
+  const addBtn = document.createElement("button");
+  addBtn.className = "todo-action";
+  addBtn.id = "add";
+  addBtn.innerHTML = "Add";
 
-  const saveBtn = document.createElement('button');
-  saveBtn.className = 'todo-action';
-  saveBtn.id = 'save';
-  saveBtn.style.marginLeft = '5px';
-  saveBtn.innerHTML = 'Save';
+  const saveBtn = document.createElement("button");
+  saveBtn.className = "todo-action";
+  saveBtn.id = "save";
+  saveBtn.style.marginLeft = "5px";
+  saveBtn.innerHTML = "Save";
 
   // append these buttons to the todoActionsDiv
   todoActionsDiv.appendChild(addBtn);
   todoActionsDiv.appendChild(saveBtn);
 
   // select the new buttons
-  const newSaveBtn = document.querySelector('#save');
-  const newAddBtn = document.querySelector('#add');
+  const newSaveBtn = document.querySelector("#save");
+  const newAddBtn = document.querySelector("#add");
 
   // when the newSaveBtn is clicked, reload the page to "save" it
-  newSaveBtn.addEventListener('click', () => {
+  newSaveBtn.addEventListener("click", () => {
     window.location.reload();
-  })
+  });
 
-  newAddBtn.addEventListener('click', () => {
+  newAddBtn.addEventListener("click", () => {
     const addTodoModal = document.querySelector("#addModal");
-    const addTodoBtn = document.querySelector('#addTodo');
+    const addTodoBtn = document.querySelector("#addTodo");
 
-    addTodoModal.style.display = 'flex';
+    addTodoModal.style.display = "flex";
 
-    addTodoBtn.addEventListener('click', () => {
-      const addTodoName = document.querySelector('#newName');
+    addTodoBtn.addEventListener("click", () => {
+      const addTodoName = document.querySelector("#newName");
 
       // get the existing task
       const todosRef = ref(database, `users/${uid}/${name}/todos`);
@@ -217,24 +248,29 @@ const editTodo = (uid, name) => {
 
       onValue(todosRef, (snapshot) => {
         const data = snapshot.val();
-        
+
         console.log(data);
 
         for (let num in data) {
-          indexNum = num
+          indexNum = num;
         }
-      })
-      set(ref(database, `/users/${uid}/${name}/todos/${Number(indexNum) + 1}`), {
-        "name": addTodoName.value
-      })
-      alert('Added the new todo');
+      });
+      set(
+        ref(database, `/users/${uid}/${name}/todos/${Number(indexNum) + 1}`),
+        {
+          name: addTodoName.value,
+        },
+      );
+      alert("Added the new todo");
       window.location.reload();
-    })
-  })
-}
+      
+    });
+  });
+  */
+};
 
 const deleteTodo = (uid, name) => {
-  set(ref(database, `users/${uid}/${name}`), null)
-  alert('Deleted the todo list');
+  set(ref(database, `users/${uid}/${name}`), null);
+  alert("Deleted the todo list");
   window.location.reload();
-}
+};
